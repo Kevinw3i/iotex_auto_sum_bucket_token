e0b7cc32969dfe4783ff2ff5a2ce964e676d167f
Kevin <k3vinwei@gmail.com>
2022-06-07 22:29:07 +0800

    feat: v1 update v2 version query

--- bucket.rb
+++ bucket.rb
@@ -30,18 +30,19 @@ end
 
 class Tools::Api
   # IoTexScan
-  GET_BUCKET_BY_QUERY = 'https://analytics.iotexscan.io/query'
-  BUCKET_LIST = [0]
+  GET_BUCKET_BY_QUERY = 'https://v2.iotexscan.io/api/services/bucket/queries/getBucketsData'
+  BUCKET_LIST = []
   BASE_AMOUNT = 1000000000000000000
+  ACT_TYPE = ['StakeCreate', 'DepositToStake']
 
-  SEARCH_SKIP = 0
-  SEARCH_COUNT = 500
+  SEARCH_SKIP = 1
+  SEARCH_COUNT = 1000
 
   class << self
     def perform
       BUCKET_LIST.each do |bucket|
         @page_count = 0
-        @skip = SEARCH_SKIP.dup
+        @page = SEARCH_SKIP.dup
         @search_count = SEARCH_COUNT.dup
         @break_switch = false
 
@@ -49,21 +50,19 @@ class Tools::Api
           get_stake_list(bucket)
           break if @break_switch
           @page_count += 1
-          @skip = @search_count * @page_count
           puts "Bucket[#{bucket}](ongoing): #{variable_get(bucket)}"
           sleep(1)
         end
       end
 
-      puts "DONE: "
       BUCKET_LIST.each do |bucket|
-        puts "Bucket[#{bucket}]: #{variable_get(bucket)}"
+        puts "Bucket[#{bucket}](DONE): #{variable_get(bucket)}"
       end
     end
 
     def get_stake_list(bucket)
       begin
-        json_body = %Q[{"operationName":"action","variables":{"bucketIndex":#{bucket},"pagination":{"skip":#{@skip},"first":#{@search_count}}},"query":"query action($bucketIndex: Int!, $pagination: Pagination!) {  action {    byBucketIndex(bucketIndex: $bucketIndex) {      count      actions(pagination: $pagination) {        actHash        blkHash        timeStamp        actType        sender        recipient        amount        gasFee        __typename      }      __typename    }    __typename  }}"}]
+        json_body = %Q[{"params":{"page":#{@page_count},"limit":#{@search_count},"bucket_id":#{bucket}},"meta":{}}]
 
         uri = URI.parse("#{GET_BUCKET_BY_QUERY}")
         request = Net::HTTP::Post.new(uri)
@@ -81,8 +80,8 @@ class Tools::Api
 
         parsed_json = JSON.parse(response.body).with_indifferent_access
 
-        @break_switch = true and return if parsed_json[:data][:action][:byBucketIndex].nil?
-        actions = parsed_json[:data][:action][:byBucketIndex][:actions]
+        @break_switch = true and return if parsed_json[:result][:buckets].blank?
+        actions = parsed_json[:result][:buckets]
         actions_count_amount(actions, bucket)
       rescue => exception
         puts exception
@@ -91,7 +90,7 @@ class Tools::Api
 
     def actions_count_amount(actions, bucket)
       bucket_amount = variable_get(bucket)
-      actions.each { |action| bucket_amount = bucket_amount + (action[:amount].to_f/BASE_AMOUNT) }
+      actions.each { |action| bucket_amount = bucket_amount + (action[:amount].to_f/BASE_AMOUNT) if ACT_TYPE.include?(action[:act_type]) }
       build_variable_set(bucket, bucket_amount)
     end
 
@@ -100,7 +99,7 @@ class Tools::Api
     end
 
     def variable_get(bucket)
-      instance_variables.include?("@bucket_#{bucket}".to_sym) ? instance_variable_get("@bucket_#{bucket}".to_sym) : build_variable_set(bucket, 0)
+      instance_variables.include?("@bucket_#{bucket}".to_sym) ? instance_variable_get("@bucket_#{bucket}".to_sym) : build_variable_set(bucket, 0.0)
     end
   end
 end
